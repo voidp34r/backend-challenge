a97de13c361bc1eae8cb08d7f8d9615850e97c97
Void P34r <matheus@matheusr.com>
2019-01-25 15:38:53 -0300

    adicionando User controller

---
+++ src/main/java/com/invillia/acme/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.invillia.acme.controllers;
+
+import com.invillia.acme.models.User;
+import com.invillia.acme.models.UserDto;
+import com.invillia.acme.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping(value="/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    //@Secured({"ROLE_ADMIN", "ROLE_USER"})
+    @PreAuthorize("hasRole('ADMIN')")
+    @RequestMapping(value="/users", method = RequestMethod.GET)
+    public List<User> listUser(){
+        return userService.findAll();
+    }
+
+    //@Secured("ROLE_USER")
+    @PreAuthorize("hasRole('USER')")
+    ////@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public User getOne(@PathVariable(value = "id") Long id){
+        return userService.findById(id);
+    }
+
+
+    @RequestMapping(value="/signup", method = RequestMethod.POST)
+    public User saveUser(@RequestBody UserDto user){
+        return userService.save(user);
+    }
+
+
+
+}
