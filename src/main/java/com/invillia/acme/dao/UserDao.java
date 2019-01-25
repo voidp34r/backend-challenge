97232258a10f2d5d32cd24a3aeae0084d6121a1d
Void P34r <matheus@matheusr.com>
2019-01-25 15:39:17 -0300

    adicionando UserDao

---
+++ src/main/java/com/invillia/acme/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.invillia.acme.dao;
+
+import com.invillia.acme.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
