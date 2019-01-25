443ded3ff8f550910bedb16dd36d9c3320cee9b4
Void P34r <matheus@matheusr.com>
2019-01-25 15:43:09 -0300

    configurando userservice bean

--- src/main/java/com/invillia/acme/config/WebSecurityConfig.java
+++ src/main/java/com/invillia/acme/config/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.invillia.acme.config;
 
+import javax.annotation.Resource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,39 +12,45 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Resource(name = "userService")
+	private UserDetailsService userDetailsService;
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Autowired
+	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	private static final String[] AUTH_WHITELIST = {
-	// -- swagger ui
-	"/v2/api-docs",
-	"/swagger-resources",
-	"/swagger-resources/**",
-	"/configuration/ui",
-	"/configuration/security",
-	"/swagger-ui.html",
-	"/webjars/**",
-	"/static/**",
-	"/resources/**"
-	// other public endpoints of your API may be appended to this array
+			// -- swagger ui
+			"/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+			"/configuration/security", "/swagger-ui.html", "/webjars/**", "/static/**", "/resources/**"
+			// other public endpoints of your API may be appended to this array
 	};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	http
-	.authorizeRequests()
-	.antMatchers("/", "/home", "/login", "/logout").permitAll()
-	.antMatchers(AUTH_WHITELIST).permitAll()
-	.antMatchers("/hello").authenticated()
-	.antMatchers("/store/").permitAll()
-	.and()
-	.formLogin()
-	.loginPage("/login").permitAll()
-	.and()
-	.logout().permitAll();
+		http.authorizeRequests().antMatchers("/", "/home", "/login", "/logout").permitAll().antMatchers(AUTH_WHITELIST)
+				.permitAll().antMatchers("/hello").authenticated().antMatchers("/store/").permitAll().and().formLogin()
+				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	@Bean
