4b5c12e0339cfc13a2f3f821769d73eea33d05c8
Void P34r <matheus@matheusr.com>
2019-01-26 10:34:44 -0300

    fix de store model

--- src/main/java/com/invillia/acme/models/Store.java
+++ src/main/java/com/invillia/acme/models/Store.java
@@ -1,15 +1,15 @@
 package com.invillia.acme.models;
 
 import java.time.ZonedDateTime;
-import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -26,49 +26,44 @@ public class Store {
 	@Column(name="name")
 	private String name;
 	
-	@Column(name="status")
-	private String status;
-	
 	@Column(name="andress")
 	private String andress;
 	
 	@Column(name="datatime")
 	private ZonedDateTime datatime;
-
-	@ManyToMany
-	@ElementCollection(fetch = FetchType.LAZY)
-    @CollectionTable(name = "orders")
-	@JoinColumn(name = "id", nullable = false)
-	@Column(name="orders")
-	private Set<Order> orders = new HashSet<>();
 	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@Column(name="orders")
+    private Set<Order> orders;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
+
 	public String getAndress() {
 		return andress;
 	}
+
 	public void setAndress(String andress) {
 		this.andress = andress;
 	}
+
 	public ZonedDateTime getDatatime() {
 		return datatime;
 	}
+
 	public void setDatatime(ZonedDateTime datatime) {
 		this.datatime = datatime;
 	}
@@ -76,7 +71,9 @@ public class Store {
 	public Set<Order> getOrders() {
 		return orders;
 	}
+
 	public void setOrders(Set<Order> orders) {
 		this.orders = orders;
 	}
+	
 }
