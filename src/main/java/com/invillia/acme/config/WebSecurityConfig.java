740f9f35042598e6c731ffe2d9078a3b82487c2c
Void P34r <matheus@matheusr.com>
2019-01-26 10:32:00 -0300

    incluindo filtro CORS nas configurações web

--- src/main/java/com/invillia/acme/config/WebSecurityConfig.java
+++ src/main/java/com/invillia/acme/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package com.invillia.acme.config;
 
+import java.util.Arrays;
+
 import javax.annotation.Resource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +45,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 	private static final String[] AUTH_WHITELIST = {
 			// -- swagger ui
 			"/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
@@ -48,16 +66,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/signup","/home", "/login", "/logout").permitAll().antMatchers(AUTH_WHITELIST)
-				.permitAll()
-				.antMatchers("/api/users/").hasAnyRole("ADMIN")
+		http.authorizeRequests().antMatchers("/", "/signup", "/home", "/login", "/logout").permitAll()
+				.antMatchers(AUTH_WHITELIST).permitAll().antMatchers("/api/users/").hasAnyRole("ADMIN")
 				.antMatchers("/api/users/**").hasAnyRole("ADMIN", "USER")
-				.anyRequest().authenticated()
-				.and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll()
-				.and()
-				.httpBasic()
-				.authenticationEntryPoint(authEntryPoint);
+				// .antMatchers("/api/stores/**").permitAll()
+				// .antMatchers("/api/orders/**").permitAll()
+				.antMatchers("/api/stores/").hasAnyRole("ADMIN").antMatchers("/api/stores/**")
+				.hasAnyRole("ADMIN", "USER")
+				// .antMatchers("/api/orders/").hasAnyRole("ADMIN")
+				// .antMatchers("/api/orders/**").hasAnyRole("ADMIN", "USER")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll().and().rememberMe().tokenValiditySeconds(60).and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint).and().cors().and().csrf().disable();
+	}
+
+	@Bean
+	public FilterRegistrationBean CorsFilterBean() {
+		final FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+		filterRegBean.setFilter(new CORSFilter());
+		filterRegBean.addUrlPatterns("/*");
+		filterRegBean.setEnabled(Boolean.TRUE);
+		filterRegBean.setName("Meu Filter");
+		filterRegBean.setAsyncSupported(Boolean.TRUE);
+		return filterRegBean;
 	}
 
 }
