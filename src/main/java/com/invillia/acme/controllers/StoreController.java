998d322687a6875cc06689563e6dad9cee12603d
Void P34r <matheus@matheusr.com>
2019-01-26 10:32:46 -0300

    fix store controller

--- src/main/java/com/invillia/acme/controllers/StoreController.java
+++ src/main/java/com/invillia/acme/controllers/StoreController.java
@@ -1,23 +1,18 @@
 package com.invillia.acme.controllers;
 
-import java.net.URI;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
 import com.invillia.acme.models.Store;
 import com.invillia.acme.repository.StoreRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @PreAuthorize("hasAnyRole('ADMIN','USER')")
 @RequestMapping(value = "/api")
@@ -36,8 +31,38 @@ public class StoreController {
     @Secured("ROLE_USER")
     @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @RequestMapping(value = "/stores/{id}", method = RequestMethod.GET)
-    public Optional<Store> getOne(@PathVariable(value = "id") Long id) {
+    public Optional<Store> getOne(@PathVariable(value = "id") Integer id) {
         return storeRepository.findById(id);
+    }
+
+    @Secured("ROLE_USER")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/stores/addstore", method = RequestMethod.POST)
+    public Store addStore(@RequestBody Store store) {
+        ZoneId fusoHorario = ZoneId.of("America/Sao_Paulo");
+		ZonedDateTime dateTime = ZonedDateTime.now(fusoHorario);
+        store.setDatatime(dateTime);
+        return storeRepository.save(store);
+    }
+
+    @Secured("ROLE_USER")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/stores/update", method = RequestMethod.PUT)
+    public Store updateStore(@RequestBody Store store) {
+        Store ns = new Store();
+        ns.setId(store.getId());
+        ns.setName(store.getName());
+        ns.setAndress(store.getAndress());
+        return storeRepository.save(ns);
+    }
+
+    @Secured("ROLE_USER")
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @RequestMapping(value = "/stores/delete", method = RequestMethod.DELETE)
+    public Store deleteStore(@RequestBody Store store) {
+        Store delStore = storeRepository.findById(store.getId()).get();
+        storeRepository.delete(storeRepository.findById(store.getId()).get());
+        return delStore;
     }
 
 }
