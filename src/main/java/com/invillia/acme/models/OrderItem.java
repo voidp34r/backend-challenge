bb513ee59ebfe100b2af4d757e7626f1c1bda050
Void P34r <matheus@matheusr.com>
2019-01-26 10:41:11 -0300

    fix orderitem model

--- src/main/java/com/invillia/acme/models/OrderItem.java
+++ src/main/java/com/invillia/acme/models/OrderItem.java
@@ -1,61 +1,61 @@
-// package com.invillia.acme.models;
+package com.invillia.acme.models;
 
-// import javax.persistence.Column;
-// import javax.persistence.Entity;
-// import javax.persistence.GeneratedValue;
-// import javax.persistence.GenerationType;
-// import javax.persistence.Id;
-// import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-// @Entity
-// @Table(name="orderItem")
-// @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-// public class OrderItem {
+@Entity
+@Table(name="orderItem")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class OrderItem {
 	
-// 	@Id
-//     @GeneratedValue(strategy=GenerationType.AUTO)
-//     private Integer id;
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
 	
-// 	@Column(name="description")
-// 	private String description;
+	@Column(name="description")
+	private String description;
 	
-// 	@Column(name="unitPrice")
-// 	private Double unitPrice;
+	@Column(name="unitPrice")
+	private Double unitPrice;
 	
-// 	@Column(name="quantity")
-// 	private Integer quantity;
+	@Column(name="quantity")
+	private Integer quantity;
 	
-// 	public Integer getId() {
-// 		return id;
-// 	}
+	public Integer getId() {
+		return id;
+	}
 
-// 	public void setId(Integer id) {
-// 		this.id = id;
-// 	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-// 	public String getDescription() {
-// 		return description;
-// 	}
+	public String getDescription() {
+		return description;
+	}
 
-// 	public void setDescription(String description) {
-// 		this.description = description;
-// 	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-// 	public Double getUnitPrice() {
-// 		return unitPrice;
-// 	}
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
 
-// 	public void setUnitPrice(Double unitPrice) {
-// 		this.unitPrice = unitPrice;
-// 	}
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
 
-// 	public Integer getQuantity() {
-// 		return quantity;
-// 	}
+	public Integer getQuantity() {
+		return quantity;
+	}
 
-// 	public void setQuantity(Integer quantity) {
-// 		this.quantity = quantity;
-// 	}
-// }
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+}
